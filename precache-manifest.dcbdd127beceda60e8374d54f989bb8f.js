22b98134d2d93697cec61896a6ba404f1ec45600
vaibhavnarkhede12 <vaibhavnarkhede12@gmail.com>
2020-03-18 00:35:57 +0530

    Updates

---
+++ precache-manifest.dcbdd127beceda60e8374d54f989bb8f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2fc6d785f5baeea214366f207f22ba70",
+    "url": "/ghpagesfypapp/index.html"
+  },
+  {
+    "revision": "c02c0e88e5f5f16b1823",
+    "url": "/ghpagesfypapp/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "66c9a4ace4bcdca5bcde",
+    "url": "/ghpagesfypapp/static/js/2.1c9c1a60.chunk.js"
+  },
+  {
+    "revision": "e9e3f0753e982b06112da7a2fd8ca3b5",
+    "url": "/ghpagesfypapp/static/js/2.1c9c1a60.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c02c0e88e5f5f16b1823",
+    "url": "/ghpagesfypapp/static/js/main.615cc2e5.chunk.js"
+  },
+  {
+    "revision": "d6788633a3310792b310",
+    "url": "/ghpagesfypapp/static/js/runtime-main.5af41a81.js"
+  }
+]);
